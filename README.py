e366fc4ba0ebfb40c0802ef43ea55cd406be763d
Sudhip Nashi <22717474+sunflsks@users.noreply.github.com>
2024-07-11 22:40:02 +0530

    add readme

---
+++ README.py
@@ -0,0 +1,5 @@
+# word hunt solver
+
+i got bored in india and i wanted to beat my friend in word hunt (but beat them of my own accord) so i created this program
+
+learned about tries, very interesting. also DAFSAs. maybe update this in the future to use DAFSA?
