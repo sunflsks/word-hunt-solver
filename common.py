afd9b47c0f4103d46ca8d18e573f18def8991a69
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2024-08-20 14:32:19 -0500

    change dict

--- common.py
+++ common.py
@@ -4,4 +4,4 @@ import itertools
 
 def ingest_dictionary():
     print("Ingesting dictionary...")
-    return {x.strip().lower() for x in open("words.dict", "r+")}
+    return {x.strip().lower() for x in open("enable1.dict", "r+")}
